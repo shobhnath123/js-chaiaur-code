370ad93f848be47a37e31477e80c4633c019c50b
Shobhnath <38579204+shobhnath123@users.noreply.github.com>
2025-05-03 17:16:54 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "shobhnat"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueIntNumber = Number(score);
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+
+
+//"33" => 33
+// "33abc"=>NaN
+//true =>1
+
+// let isLoggedIn = "ff"
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shobhnath" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
