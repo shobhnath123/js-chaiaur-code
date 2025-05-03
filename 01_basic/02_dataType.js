6206f411c9eb4580eed0283aa2f020c85f4d17ec
Shobhnath <38579204+shobhnath123@users.noreply.github.com>
2025-05-03 12:26:14 +0000

    datatype

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello World"); // we  are using node js not browser
+
+
+console.log( 3 + 
+    3) // code readability should be good
+console.log("object")
+
+
+let name = "Shobhnath"
+let age = 23
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => empty value (standlone value)
+// undefined => variable is declared but not assigned
+// symbol => unique
+
+
+// object => collection of key value pair
+
+console.log(typeof isLoggedIn)
